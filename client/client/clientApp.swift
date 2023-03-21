a141c073f8fc39b01c7ff5807ab4b4a28c499fe7
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-21 11:13:49 -0300

    first commit

---
+++ client/client/clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  clientApp.swift
+//  client
+//
+//  Created by matheusvb on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
