93634776cd40a141e691b2aa6330cdd33390c155
Matheus Batista <matheus.vbatista04@gmail.com>
2023-03-21 11:53:11 -0300

    feature: socket client service setup

---
+++ client/client/SocketService.swift
@@ -0,0 +1,39 @@
+//
+//  SocketService.swift
+//  client
+//
+//  Created by matheusvb on 21/03/23.
+//
+
+import Foundation
+import SwiftUI
+import SocketIO
+
+final class SocketService: ObservableObject {
+    let manager = SocketManager(socketURL: URL(string: "http://localhost:5050")!, config: [.log(true), .compress])
+    
+    @Published var socket: SocketIOClient
+    @Published var lines: [Line] = []
+    
+    init() {
+        socket = manager.defaultSocket
+        
+        socket.on("connect") { (data, ack) in
+            print("Connected")
+        }
+        
+        socket.on("download_lines") { (data, ack) in
+            if let buffer = data[0] as? String {
+                let dataDecoded : Data = Data(base64Encoded: buffer, options: .ignoreUnknownCharacters)!
+                do {
+                    let dataArray = try JSONSerialization.jsonObject(with: dataDecoded, options: []) as! [Line]
+                    self.lines = dataArray
+                } catch {
+                    print(error)
+                }
+            }
+        }
+        
+        socket.connect()
+    }
+}
